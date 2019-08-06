5d52a52117f0cc813bf21638270fcf4c98bb4981
mavennn <leshich417@gmail.com>
2019-08-06 21:38:36 +0300

    prop-types module added

--- src/server/queries.js
+++ src/server/queries.js
@@ -1,41 +1,42 @@
-const Pool = require('pg').Pool
+const { Pool } = require('pg');
+
 const pool = new Pool({
-	user: 'gadoevalex',
-	host: 'localhost',
-	database: 'sela',
-	password: '',
-	port: 5432,
-})
+  user: 'gadoevalex',
+  host: 'localhost',
+  database: 'sela',
+  password: '',
+  port: 5432,
+});
 
 const getThingByVendorCode = (request, response) => {
-    const vendor_code = request.params.vendor_code.toString();
-    pool.query('SELECT * FROM things WHERE vendor_code = $1', [vendor_code], (err, thing) => {
-        if (err) throw err;
-        // console.log(thing.rows)
-        response.status(200).json(thing.rows[0])
-    });
+  const vendorCode = request.params.vendor_code.toString();
+  pool.query('SELECT * FROM things WHERE vendor_code = $1', [vendorCode], (err, thing) => {
+    if (err) throw err;
+    console.log(thing.rows);
+    response.status(200).json(thing.rows[0]);
+  });
 };
 
 const getSizesById = (request, response) => {
-    const id = parseInt(request.params.id);
-    pool.query('SELECT * FROM thing_sizes WHERE id_thing = $1', [id], (err, sizes) => {
-        if (err) throw err;
-        var thing_sizes = [];
-        sizes.rows.forEach(i => thing_sizes.push(i.title));
-        response.status(200).json(thing_sizes)
-    });
-}
+  const id = parseInt(request.params.id, 10);
+  pool.query('SELECT * FROM thing_sizes WHERE id_thing = $1', [id], (err, sizes) => {
+    if (err) throw err;
+    const thingSizes = [];
+    sizes.rows.forEach(i => thingSizes.push(i.title));
+    response.status(200).json(thingSizes);
+  });
+};
 
 const getRecommendationByCapsuleId = (request, response) => {
-    const id_capsule = parseInt(request.params.id_capsule);
-    pool.query('SELECT * FROM things WHERE id_capsule = $1', [id_capsule], (err, things) => {
-        if (err) throw err;
-        response.status(200).json(things.rows)
-    })
-}
+  const idCapsule = parseInt(request.params.id_capsule, 10);
+  pool.query('SELECT * FROM things WHERE id_capsule = $1', [idCapsule], (err, things) => {
+    if (err) throw err;
+    response.status(200).json(things.rows);
+  });
+};
 
 module.exports = {
-    getThingByVendorCode,
-    getSizesById,
-    getRecommendationByCapsuleId,
-}
+  getThingByVendorCode,
+  getSizesById,
+  getRecommendationByCapsuleId,
+};
