8de8e1dbe434402c1e1f0ed8e23bd0899d722c60
mavennn <leshich417@gmail.com>
2019-08-06 21:37:50 +0300

    naming

--- src/client/actions/items.js
+++ src/client/actions/items.js
@@ -4,37 +4,27 @@ export const ADD_TO_BASKET = 'ADD_TO_BASKET';
 export const SET_TO_DEFAULT = 'SET_TO_DEFAULT';
 export const ADD_TO_CAPSULE = 'ADD_TO_CAPSULE';
 
-export const setCurrenItem = (item) => {
-   return { 
-       type: 'ADD_CURR_ITEM', 
-       payload: item
-    };
-}  
+export const setCurrentItem = item => ({
+  type: 'ADD_CURR_ITEM',
+  payload: item
+});
 
-export const addToHistory = (item) => {
-    return {
-        type: 'ADD_TO_HISTORY',
-        payload: item
-    }
-}
+export const addToHistory = item => ({
+  type: 'ADD_TO_HISTORY',
+  payload: item
+});
 
-export const addToBasket = (item) => {
-    return {
-        type: 'ADD_TO_BASKET',
-        payload: item
-    }
-}
+export const addToBasket = item => ({
+  type: 'ADD_TO_BASKET',
+  payload: item
+});
 
-export const addToCapsule = (items) => {
-    return {
-        type: 'ADD_TO_CAPSULE',
-        payload: items
-    }
-}
+export const addToCapsule = items => ({
+  type: 'ADD_TO_CAPSULE',
+  payload: items
+});
 
-export const setToDefault = () => {
-    return {
-        type: 'SET_TO_DEFAULT',
-        payload: undefined
-    }
-}
+export const setToDefault = () => ({
+  type: 'SET_TO_DEFAULT',
+  payload: undefined
+});
