ac3f6db10fa48afcd12d1bde389b83dd3d8d403b
mavennn <leshich417@gmail.com>
2019-08-06 21:33:47 +0300

    from class to pure func

--- src/client/components/Basket.js
+++ src/client/components/Basket.js
@@ -1,56 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import Header from './Header';
 import BarCode from './BarCodeInput';
-import { setCurrenItem } from '../actions/items';
+import { setCurrentItem } from '../actions/items';
 
-class Basket extends React.Component{
+const Basket = ({ basketItems, setCurrentItem }) => {
+  if (basketItems.length !== 0) {
+    return (
+      <div id="basket">
+        <Header />
+        <ul>
+          {
+            basketItems.map(item => (
+              <li key={item.bar_code}>
+                <img
+                  src={require(`../assets/img/${item.vendor_code}-1.png`)}
+                  alt="img"
+                />
+                <p>{item.title}</p>
+              </li>
+            ))
+          }
+        </ul>
+        <BarCode />
+      </div>
+    );
+  }
+  return (
+    <div id="basket">
+      <Header />
+      <BarCode />
+    </div>
+  );
+};
 
-    render(){
-        const  basketItems = this.props.basketItems;
-        if(basketItems.length != 0) {
-            return(
-                <div id="basket">
-                    <Header />
-                    <ul>
-                    {
-                        basketItems.map(item => {
-                            return(
-                                <li key={item.bar_code}>
-                                    <img 
-                                        src={require(`../assets/img/${item.vendor_code}-1.png`)} 
-                                    />
-                                    <p>{item.title}</p>
-                                </li>
-                            )
-                        })
-                    }
-                    </ul>
-                    <BarCode />
-                </div>
-            );
-        } else {
-            return(
-                <div id="basket">
-                    <Header />
-                    <BarCode />
-                </div>
-            );
-        }
-    }
-}
-
-const mapStateToProps = state => {
-    return {
-        basketItems: state.items.basketItems,
-    }
-}
+const mapStateToProps = state => ({
+  basketItems: state.items.basketItems,
+});
 
 const mapDispatchToProps = {
-    setCurrenItem
-}
+  setCurrentItem
+};
+
+Basket.defaultProps = {
+  basketItems: PropTypes.arrayOf,
+};
+
+Basket.propTypes = {
+  basketItems: PropTypes.arrayOf,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Basket);
