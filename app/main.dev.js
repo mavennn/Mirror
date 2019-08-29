fa1eb29f6e6b486026241ec44740722af15840bd
AGadoev <AGadoev@croc.ru>
2019-08-29 12:40:53 +0300

    autodetect serialport

--- app/main.dev.js
+++ app/main.dev.js
@@ -4,12 +4,10 @@ import log from 'electron-log';
 import MenuBuilder from './menu';
 import routes from './constants/routes';
 
-const { clipboard } = require('electron')
+const { clipboard } = require('electron');
+const SerialPort = require('serialport');
 require('dotenv').config();
 
-const SerialPort = require('serialport');
-
-const serialPortName = process.env.SERIAL_PORT;
 
 export default class AppUpdater {
   constructor() {
@@ -56,69 +54,74 @@ app.on('window-all-closed', () => {
 });
 
 app.on('ready', async () => {
-  // const serialPort = new SerialPort(serialPortName, {
-  //   baudRate: 9600, // бит в секунду
-  //   dataBits: 8, // биты данных
-  //   parity: 'none', // четность
-  //   stopBits: 1, // стоповые
-  // });
+  SerialPort.list()
+    .then(ports => {
+      let serialPortName = ports[ports.findIndex(x => x.vendorId === '23d0')].comName
+      console.log(serialPortName)
+      const serialPort = new SerialPort(serialPortName, {
+        baudRate: 9600, // бит в секунду
+        dataBits: 8, // биты данных
+        parity: 'none', // четность
+        stopBits: 1, // стоповые
+      });
+        // if (
+        //   process.env.NODE_ENV === 'development'
+        //   || process.env.DEBUG_PROD === 'true'
+        // ) {
+        //   await installExtensions();
+        // }
+        
+        mainWindow = new BrowserWindow({
+          show: false,
+          width: 1024,
+          height: 728
+        });
+      
+        mainWindow.setFullScreen(true);
+      
+        mainWindow.loadURL(`file://${__dirname}/app.html#${routes.EXPECTATION}`);
+      
+        mainWindow.webContents.on('did-finish-load', () => {
+          if (!mainWindow) {
+            throw new Error('"mainWindow" is not defined');
+          }
+          if (process.env.START_MINIMIZED) {
+            mainWindow.minimize();
+          } else {
+            mainWindow.show();
+            mainWindow.focus();
+          }
+        });
+      
+        mainWindow.on('closed', () => {
+          mainWindow = null;
+        });
+      
+        const menuBuilder = new MenuBuilder(mainWindow);
+        menuBuilder.buildMenu();
+      
+        serialPort.on('open', () => {
+          console.log(`Scanner: Open serial port ${serialPortName}`);
+          serialPort.on('data', (data) => {
+            const msg = data.toString('utf8').substr(0, Math.max(0, data.length - 1));
+            if (msg && msg.length > 0) {
+              console.log(`Scanner: vendor_code is ${msg}`);
+              mainWindow.webContents.send('vendorCode', msg);
+            } else {
+              console.log('Scanner: vendor_code is empty');
+            }
+          });
+        });
+      
+        new AppUpdater();
 
-  // ввод barcode через ctrl V
-  globalShortcut.register('CommandOrControl+V', () => {
-    const barcode = clipboard.readText();
-    console.log(barcode);
-    // mainWindow.webContents.send('vendorCode', barcode);
-  });
-
-  if (
-    process.env.NODE_ENV === 'development'
-    || process.env.DEBUG_PROD === 'true'
-  ) {
-    await installExtensions();
-  }
-  
-    mainWindow = new BrowserWindow({
-      show: false,
-      width: 1024,
-      height: 728
+        
+      });
     });
 
-
-  mainWindow.setFullScreen(true);
-
-  mainWindow.loadURL(`file://${__dirname}/app.html#${routes.EXPECTATION}`);
-
-  mainWindow.webContents.on('did-finish-load', () => {
-    if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined');
-    }
-    if (process.env.START_MINIMIZED) {
-      mainWindow.minimize();
-    } else {
-      mainWindow.show();
-      mainWindow.focus();
-    }
-  });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-
-  const menuBuilder = new MenuBuilder(mainWindow);
-  menuBuilder.buildMenu();
-
-  // serialPort.on('open', () => {
-  //   console.log(`Scanner: Open serial port ${serialPortName}`);
-  //   serialPort.on('data', (data) => {
-  //     const msg = data.toString('utf8').substr(0, Math.max(0, data.length - 1));
-  //     if (msg && msg.length > 0) {
-  //       console.log(`Scanner: vendor_code is ${msg}`);
-  //       mainWindow.webContents.send('vendorCode', msg);
-  //     } else {
-  //       console.log('Scanner: vendor_code is empty');
-  //     }
-  //   });
-  // });
-
-  new AppUpdater();
-});
+  // // ввод barcode через ctrl V
+  // globalShortcut.register('CommandOrControl+V', () => {
+  //   const barcode = clipboard.readText();
+  //   console.log(barcode);
+  //   // mainWindow.webContents.send('vendorCode', barcode);
+  // })
