0d9083f56dd43adc1fb63312f4963ba9cfeb4ef9
mavennn <leshich417@gmail.com>
2019-10-10 17:20:13 +0300

    fix basket bug

--- app/components/Basket/BasketList.js
+++ app/components/Basket/BasketList.js
@@ -2,32 +2,34 @@ import React from 'react';
 import routes from '../../constants/routes';
 import * as trash from '../../assets/icons/trash.svg';
 
-const BasketList = ({ basketThings, setThing, history, remove }) => (
-  <ul className="basketList">
-    {
-          basketThings.map(item => (
-            <li key={item.barcode + item.size}>
-              <img
-                src={item.img_base64}
-                onClick={() => {
-                  setThing(item.barcode);
-                  history.push(routes.HOME);
-                }}
-              />
-              <div className="basket-description">
-                <p className="title">{item.title}</p>
-                <em className="brand">{item.brand}</em>
-                <p className="size">{item.size}</p>
-                <p className="price">{item.price}</p>
-                <p className="color">{item.color}</p>
-                <div className="remove">
-                  <img src={trash} alt="trash" onClick={() => remove(item)} />
+const BasketList = ({ basketThings, setThing, history, remove }) => {
+  return (
+    <ul className="basketList">
+      {
+            basketThings.map(item => (
+              <li key={item._barcode + item._size}>
+                <img
+                  src={item._img_base64}
+                  onClick={() => {
+                    setThing(item._barcode);
+                    history.push(routes.HOME);
+                  }}
+                />
+                <div className="basket-description">
+                  <p className="title">{item._title}</p>
+                  <em className="brand">{item._brand}</em>
+                  <p className="size">{item._size}</p>
+                  <p className="price">{item._price}</p>
+                  <p className="color">{item._color}</p>
+                  <div className="remove">
+                    <img src={trash} alt="trash" onClick={() => remove(item)} />
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))
-        }
-  </ul>
-);
+              </li>
+            ))
+          }
+    </ul>
+  );
+};
 
 export default BasketList;
